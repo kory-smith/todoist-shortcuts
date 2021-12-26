f451979a3c16ee2c22904c7c04471fec6f086ffa
Michael Sloan <mgsloan@gmail.com>
2021-12-25 17:04:32 -0700

    Update to extension manifest v3

--- src/inject.js
+++ src/inject.js
@@ -1,6 +1,7 @@
 // Injects the main script. This is necessary in order to bind a
 // keyhandler.
 
+const browser = window.browser || window.chrome;
 const scriptEl = document.createElement('script');
-scriptEl.setAttribute('src', chrome.extension.getURL('todoist-shortcuts.js'));
+scriptEl.setAttribute('src', browser.runtime.getURL('todoist-shortcuts.js'));
 document.getElementsByTagName('body')[0].appendChild(scriptEl)
